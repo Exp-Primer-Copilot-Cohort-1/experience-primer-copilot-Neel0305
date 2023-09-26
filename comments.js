507aa7b3322d741fbd1bd3b5cb00de4046efaf2f
Neelkant Mani Upadhyay <94377216+Neel0305@users.noreply.github.com>
2023-09-26 20:13:15 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,115 @@
+// Create web server
+
+
+
+
+
+
+
+// Require the http module
+const http = require('http');
+
+// Require the url module
+const url = require('url');
+
+// Require the querystring module
+const qs = require('querystring');
+
+// Require the filesystem module
+const fs = require('fs');
+
+// Require the path module
+const path = require('path');
+
+// Require the mime-types module
+const mime = require('mime-types');
+
+// Require the comments module
+const comments = require('./comments');
+
+// Require the template module
+const template = require('./template');
+
+// Require the template module
+const template2 = require('./template2');
+
+// Require the template module
+const template3 = require('./template3');
+
+// Require the template module
+const template4 = require('./template4');
+
+// Require the template module
+const template5 = require('./template5');
+
+// Require the template module
+const template6 = require('./template6');
+
+// Require the template module
+const template7 = require('./template7');
+
+// Require the template module
+const template8 = require('./template8');
+
+// Require the template module
+const template9 = require('./template9');
+
+// Require the template module
+const template10 = require('./template10');
+
+// Require the template module
+const template11 = require('./template11');
+
+// Require the template module
+const template12 = require('./template12');
+
+// Require the template module
+const template13 = require('./template13');
+
+// Require the template module
+const template14 = require('./template14');
+
+// Require the template module
+const template15 = require('./template15');
+
+// Require the template module
+const template16 = require('./template16');
+
+// Require the template module
+const template17 = require('./template17');
+
+// Require the template module
+const template18 = require('./template18');
+
+// Require the template module
+const template19 = require('./template19');
+
+// Require the template module
+const template20 = require('./template20');
+
+// Require the template module
+const template21 = require('./template21');
+
+// Require the template module
+const template22 = require('./template22');
+
+// Require the template module
+const template23 = require('./template23');
+
+// Require the template module
+const template24 = require('./template24');
+
+// Require the template module
+const template25 = require('./template25');
+
+// Require the template module
+const template26 = require('./template26');
+
+// Require the template module
+const template27 = require('./template27');
+
+//
+
+
+
+
